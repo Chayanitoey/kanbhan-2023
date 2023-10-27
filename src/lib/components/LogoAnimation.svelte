b05d347f79321bfd4d25c342033061a9d6126af6
ChayTha <61849099+ChayTha@users.noreply.github.com>
2023-10-27 12:08:25 -0400

    add logo

---
+++ src/lib/components/LogoAnimation.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { fade } from "svelte/transition";
+</script>
+
+<!-- center the logo -->
+<div class="logo" in:fade={{ duration: 400 }} out:fade={{ duration: 400 }}>
+	<picture>
+		<source srcset="/svg/kanbhan_logo.svg" type="svg" />
+		<img
+			alt="Perfect Laundry Day Logo"
+			decoding="async"
+			height="100vw"
+			loading="eager"
+			src="/svg/kanbhan_icon.svg"
+			class="logo_svg"
+		/>
+	</picture>
+	<p class="kanbhan-logo">Kanbhan</p>
+</div>
+
+<style>
+	.logo {
+		margin-top: 30vh;
+		display: grid;
+		justify-content: center;
+		opacity: 0;
+	}
+	.logo_svg {
+		padding-left: 40%;
+	}
+	.kanbhan-logo {
+		margin-top: 0%;
+		font-family: var(--kbh-bricolage);
+		font-weight: 700;
+		font-size: 10vh;
+		color: var(--kbh-red);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p> -->
+
+<script>
+	import LogoAnimation from "$lib/components/LogoAnimation.svelte";
+	import "$lib/style/global.css";
+</script>
+
+<LogoAnimation />
